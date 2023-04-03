c75a944641fb945cb3bde509301c795db9c24c4a
Tom Nicholas <tomblyth90@gmail.com>
2023-04-03 12:18:52 +1000

    css & ejs layout

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const ejs = require('ejs');
 const config = require('./config')
+const expressLayouts = require('express-ejs-layouts');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-})
+app.use(express.static("public"))
 
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+
+app.get('/', (req, res) => {
+    res.render("map");
+})
 
 app.listen(config.port, () => {
     console.log(`You are listening at port ${config.port}`);
