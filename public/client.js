ae9690b9f1f1ef2aac550cbb049b0795bfde25a8
Deepali Patro <deepaliparo86@gmail.com>
2023-04-04 10:01:05 +1000

    map center and zoom set

--- public/client.js
+++ public/client.js
@@ -5,9 +5,24 @@ async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
 
     map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 10,
+        center: { lat: -37.9338, lng: 145.0726 },
+        zoom: 13,
+        minZoom: 10,
+    });
+
+    document.getElementById('center-lat').innerHTML = 'lat: ' + map.getCenter().lat()
+    document.getElementById('center-lng').innerHTML = 'lng: ' + map.getCenter().lng()
+
+    map.addListener("center_changed", () => {
+        let center = map.getCenter();
+        let latitude = center.lat();
+        let longitude = center.lng();
+        
+        document.getElementById('center-lat').innerHTML = 'lat: ' + latitude
+        document.getElementById('center-lng').innerHTML = 'lng: ' + longitude
     });
 }
 
+
 initMap();
+
