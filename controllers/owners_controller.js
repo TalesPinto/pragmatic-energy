7b545027bb4d66103cbc1fefa115e1001720a691
Tales Pinto <tales.pinto@hotmail.com>
2023-04-04 11:36:09 +1000

    Owners controller working

--- controllers/owners_controller.js
+++ controllers/owners_controller.js
@@ -1,5 +1,14 @@
 const { Router } = require('express');
 const router = Router();
+const db = require('./../db')
 
+
+router.get('/', (req, res) => {
+    const sql = 'SELECT DISTINCT owner FROM petrol_stations;'
+
+    db.query(sql)
+        .then(dbRes => dbRes.rows)
+        .then(owners => res.json(owners))
+})
 
 module.exports = router;
