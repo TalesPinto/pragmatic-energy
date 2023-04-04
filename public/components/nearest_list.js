17fc3e22c7c28bff2da86de77c246e8a52f622fa
Deepali Patro <deepaliparo86@gmail.com>
2023-04-05 00:07:48 +1000

    added some basic css and added images to nearest stations

--- public/components/nearest_list.js
+++ public/components/nearest_list.js
@@ -17,27 +17,36 @@ function renderStationList(stations) {
 
 
 function renderStation(station) {
-    const isAmpol = (station.owner === 'Ampol');
-    const isBp = (station.owner === 'BP');
-    const isCaltex = (station.owner === 'Caltex');
-    const isShell = (station.owner === 'Shell');
-    const isSevenEleven = (station.owner === '7-Eleven Pty Ltd');
-    const isUnited = (station.owner === 'United');
-    const isGeneric = !isAmpol && !isBp && !isCaltex && !isShell && !isSevenEleven && !isUnited;
+    // const isAmpol = (station.owner === 'Ampol');
+    // const isBp = (station.owner === 'BP');
+    // const isCaltex = (station.owner === 'Caltex');
+    // const isShell = (station.owner === 'Shell');
+    // const isSevenEleven = (station.owner === '7-Eleven Pty Ltd');
+    // const isUnited = (station.owner === 'United');
+    // const isGeneric = !isAmpol && !isBp && !isCaltex && !isShell && !isSevenEleven && !isUnited;
     
-    let imgFileName = '';
+    // let imgFileName = '';
     
-    if (isAmpol) imgFileName = 'ampol.jpeg';
-    if (isBp) imgFileName = 'bp.png';
-    if (isCaltex) imgFileName = 'caltex.png';
-    if (isShell) imgFileName = 'shell.png';
-    if (isSevenEleven) imgFileName = 'seven-eleven.png';
-    if (isUnited) imgFileName = 'united.jpeg';
-    if (isGeneric) imgFileName = 'generic.jpg';
+    // if (isAmpol) imgFileName = 'ampol.jpeg';
+    // if (isBp) imgFileName = 'bp.png';
+    // if (isCaltex) imgFileName = 'caltex.png';
+    // if (isShell) imgFileName = 'shell.png';
+    // if (isSevenEleven) imgFileName = 'seven-eleven.png';
+    // if (isUnited) imgFileName = 'united.jpeg';
+    // if (isGeneric) imgFileName = 'generic.jpg';
+
+    const icons = {
+        Ampol: "/images/ampol.jpeg",
+        BP: "/images/bp.png",
+        Caltex: "/images/caltex.png",
+        Shell: "/images/shell.png",
+        '7-Eleven Pty Ltd': "/images/seven-eleven.png",
+        Generic: "/images/generic.jpg",
+    }
 
     return `
                     <li>
-                        <img src='#' alt=""> <!-- Replace the img src with actual filepath ('./../images/imgFileName) once CSS is setup -->
+                        <img src='${icons[station.owner] || icons.Generic}' alt=""> <!-- Replace the img src with actual filepath ('./../images/imgFileName) once CSS is setup -->
                         <div>
                             <div><p>${station.name}</p></div>
                             <p>${station.address}</p>
