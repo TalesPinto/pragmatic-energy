c2581f3f0cd5f56af1bc79e65f286b8eeff7f1c7
Tales Pinto <tales.pinto@hotmail.com>
2023-04-05 14:48:13 +1000

    spotlight update

--- public/client.js
+++ public/client.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const icons = {
     Ampol: "/images/ampol.jpeg",
     BP: "/images/bp.png",
@@ -18,18 +17,6 @@ async function initMap() {
 
     map = new Map(document.getElementById("map"), {
         // center: { lat: -37.9338, lng: 145.0726 },
-=======
-let map;
-
-async function initMap() {
-
-    // getUserLocation();
-
-    const { Map } = await google.maps.importLibrary("maps");
-
-    map = new Map(document.getElementById("map"), {
-        center: { lat: latitude, lng: longitude },
->>>>>>> b2016c8 (spotlight update)
         zoom: 13,
         minZoom: 10,
     });
@@ -39,10 +26,10 @@ async function initMap() {
         let userLat = position.coords.latitude;
         let userLng = position.coords.longitude;
         map.setCenter({ lat: userLat, lng: userLng });
-        
+
         centerLat.innerHTML = 'lat: ' + map.getCenter().lat()
         centerLng.innerHTML = 'lng: ' + map.getCenter().lng()
-    } 
+    }
     function error(err) {
         // User denied geolocation prompt - default to Chicago
         map.setCenter({ lat: -37.9338, lng: 145.0726 });
@@ -56,15 +43,9 @@ async function initMap() {
         let center = map.getCenter();
         let latitude = center.lat();
         let longitude = center.lng();
-<<<<<<< HEAD
-        
+
         centerLat.innerHTML = 'lat: ' + latitude
         centerLng.innerHTML = 'lng: ' + longitude
-=======
-
-        document.getElementById('center-lat').innerHTML = 'lat: ' + latitude
-        document.getElementById('center-lng').innerHTML = 'lng: ' + longitude
->>>>>>> b2016c8 (spotlight update)
     });
 
     const infoWindow = new google.maps.InfoWindow({
@@ -72,53 +53,62 @@ async function initMap() {
         // ariaLabel: station.name,
     });
 
-    axios.get("/api/stations/all").then(stations => {
+    map.addListener('idle', () => {
+        // When the map is idle, the map box's boundaries are extracted
+        const { lo: lowerLat, hi: upperLat } = map.getBounds().Va;
+        const { lo: lowerLng, hi: upperLng } = map.getBounds().Ga;
 
-        for (const station of stations.data) {
+        // An API axios request is made to retrieve the stations within the coordinate boundaries set by the map box
+        const queryString = `?lowerlat=${lowerLat}&upperlat=${upperLat}&lowerlng=${lowerLng}&upperlng=${upperLng}`
+        axios.get(`/api/stations/bounds${queryString}`).then(stations => {
 
-            const myLatLng = { lat: station.latitude, lng: station.longitude };
+            for (const station of stations.data) {
 
-            const marker = new google.maps.Marker({
-                position: myLatLng,
-                map,
-                title: station.name,
-                // label: station.name,
-            });
+                const myLatLng = { lat: station.latitude, lng: station.longitude };
 
-            const contentString = `<div><h3>${station.name}</h3> <p>${station.address}</p></div>`
+                const image = {
+                    url: icons[station.owner] || icons.Generic,
+                    scaledSize: new google.maps.Size(25, 25),
+                    origin: new google.maps.Point(0, 0), // origin
+                    anchor: new google.maps.Point(0, 0) // anchor
+                };
 
-            marker.addListener("click", () => {
-                infoWindow.close()
-                infoWindow.setContent(contentString)
-                infoWindow.open({
-                    anchor: marker,
+                const marker = new google.maps.Marker({
+                    position: myLatLng,
                     map,
+                    title: station.name,
+                    // label: station.name,
+                    icon: image
                 });
-            });
 
-            // marker.addListener("mouseover", () => {
-            //     marker.label = station.name
-            // })
-            marker.addListener('mouseover', function () {
-                infoWindow.setContent(`<h3>${station.name}</h3>`)
-                infoWindow.open({
-                    anchor: marker,
-                    map,
+                const contentString = `<div><h3>${station.name}</h3> <p>${station.address}</p></div>`
+
+                marker.addListener("click", () => {
+                    infoWindow.close()
+                    infoWindow.setContent(contentString)
+                    infoWindow.open({
+                        anchor: marker,
+                        map,
+                    });
                 });
-            });
-            marker.addListener('mouseout', function () {
-                infoWindow.close();
-            });
 
-        };
-    });
+                marker.addListener("mouseover", () => {
+                    marker.label = station.name
+                })
+                // marker.addListener('mouseover', function() {
+                //     infoWindow.setContent(`<h3>${station.name}</h3>`)
+                //     infoWindow.open({
+                //       anchor: marker,
+                //       map,
+                //     }); 
+                // });
+                // marker.addListener('mouseout', function() {
+                //     infoWindow.close();
+                // });
+            };
+        });
 
-
-<<<<<<< HEAD
     })
-=======
-
->>>>>>> b2016c8 (spotlight update)
 }
 
 initMap();
